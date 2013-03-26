a1b5f6e835ccf4abfb4f02ee20982a19fd327754
Nick Villec <vilninliv@gmail.com>
2013-03-26 08:00:50 -0700

    first commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+//
+//  hello.c
+//
+//  Author:   Nick Villec
+//  Created:  03/25/2013 14:29:10
+//  Notes:    Hello!
+//
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello World!\n");
+	
+	return 0;
+}
+ 
+
